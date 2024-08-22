aa9bdb7a7915102e9a2092d71c44de7de6b6325c
vjvijay2 <vijayanbu2002@gmail.com>
2024-08-22 08:26:47 +0530

    thd cuuppsst

--- server.js
+++ server.js
@@ -119,6 +119,7 @@
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
